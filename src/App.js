686aed4054994992e39cb27a5cd9aa70cca744c7
ankit detroja <ankit.b.detroja@gmail.com>
2022-02-18 19:23:50 +0530

    React-TODO-App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+let globalID = 0;
 
 function App() {
+  const [task, setTask] = useState(" ");
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = () => {
+    setTodos((oldTodos) => {
+      setTask("");
+      return [...oldTodos, { todo: task, id: globalID++ }];
+    });
+  };
+  const checkForEnterKey = (e) => {
+    if (e.keyCode === 13) {
+      createTodo();
+    }
+  };
+  const deleteItem = (itemID) => {
+    setTodos((oldTodos) => oldTodos.filter((item) => item.id !== itemID));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Best TODO App Ever</h2>
+      <input
+        onKeyDown={checkForEnterKey}
+        type="text"
+        value={task}
+        onChange={(event) => setTask(event.target.value)}
+        placeholder="Task"
+      ></input>
+      <button onClick={createTodo}>Create Todo</button>
+      <ul>
+        {todos.map((item, index) => {
+          return (
+            <div key={item.id}>
+              <li>{item.todo}</li>
+              <button
+                onClick={() => {
+                  deleteItem(item.id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 }
